cf6272726a14e2c1d7e1905b3d97a44897ea5cbe
Jean Boussier <jean.boussier@gmail.com>
2024-02-08 10:44:55 +0100

    Gracefully handle transactional fixtures leaks
    Extracted from: https://github.com/rails/rails/pull/50999
    
    Some tests may use the connections in ways that cause the fixtures
    transaction to be committed or rolled back. The typical case being
    doing schema change query in MySQL, which automatically commits the
    transaction. But ther eare more subtle cases.
    
    The general idea here is to ensure our transaction is correctly
    rolling back during teardown. If it fails, then we assume something
    might have mutated some of the inserted fixtures, so we invalidate
    the cache to ensure the next test will reset them.
    
    This issue is particularly common in Active Record's own test suite
    since transaction fixtures are enabled by default but we have
    many tests create tables and such.
    
    We could treat this case as an error, but since we can gracefully
    recover from it, I don't think it's worth it.

--- activerecord/test/cases/connection_adapters/connection_handler_test.rb
+++ activerecord/test/cases/connection_adapters/connection_handler_test.rb
@@ -6,6 +6,8 @@ require "models/person"
 module ActiveRecord
   module ConnectionAdapters
     class ConnectionHandlerTest < ActiveRecord::TestCase
+      self.use_transactional_tests = false
+
       fixtures :people
 
       def setup
@@ -79,6 +81,8 @@ module ActiveRecord
           connection_handler = ActiveRecord::Base.connection_handler
           ActiveRecord::Base.connection_handler = ActiveRecord::ConnectionAdapters::ConnectionHandler.new
 
+          setup_transactional_fixtures
+
           assert_nothing_raised do
             ActiveRecord::Base.connects_to(database: { reading: :arunit, writing: :arunit })
           end
@@ -87,6 +91,8 @@ module ActiveRecord
           ro_conn = ActiveRecord::Base.connection_handler.retrieve_connection("ActiveRecord::Base", role: :reading)
 
           assert_equal rw_conn, ro_conn
+
+          teardown_transactional_fixtures
         ensure
           ActiveRecord::Base.connection_handler = connection_handler
         end
